9a06f72cd4b69239b4f5503e6d6642253b1a1462
LittleboyHarry <LittleboyHarry@users.noreply.github.com>
2020-07-30 07:23:46 +0800

    refactor: 升级 Docusaurus 版本到 2.0.0-alpha.70

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
         alt: 'My Site Logo',
         src: 'img/goggles.jpg',
       },
-      links: [
+      items: [
         {
           to: 'docs/',
           activeBasePath: 'docs',
@@ -38,8 +38,6 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
-          // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
