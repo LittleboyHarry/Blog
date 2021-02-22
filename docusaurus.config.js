2249e81ed693e384b0ef5d2f1a4bc8001a5db347
LittleboyHarry <littleboyharry@users.noreply.github.com>
2021-02-22 23:52:17 +0800

    feat: 标注知识共享许可协议

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,16 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      copyright: `Built with Docusaurus.<br/>站点已使用 Google Analytics ，我想多了解一下各位亲爱的读者~`,
+      links: [
+        {
+          items: [
+            {
+              html: `<p style="text-align:center;">Built with Docusaurus.<br/>站点已使用 Google Analytics ，我想多了解一下各位亲爱的读者~</p>`,
+            },
+          ],
+        },
+      ],
+      copyright: `<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />本站点内作品采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。`,
     },
     googleAnalytics: {
       trackingID: 'UA-177209332-1',
